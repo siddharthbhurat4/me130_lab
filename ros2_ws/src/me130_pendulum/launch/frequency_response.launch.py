3ead7e4c5de2f69d61bf84451c7c1d44ac430dec
Ryan Cosner <ryan.cosner@tufts.edu>
2026-09-22 19:32:42 -0400

    Modify deadband argument description in launch file
    Updated deadband launch argument description to indicate it should be changed to a specific value.

--- ros2_ws/src/me130_pendulum/launch/frequency_response.launch.py
+++ ros2_ws/src/me130_pendulum/launch/frequency_response.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
     motor_sign = LaunchConfiguration("motor_sign")
 
     return LaunchDescription([
-        DeclareLaunchArgument("deadband", default_value="0.0",
+        DeclareLaunchArgument("deadband", default_value="0.0", # TODO: <--- Change to your deadband value
                               description="from lab 1; 0.0 means no compensation"),
         DeclareLaunchArgument("motor_sign", default_value="1.0"),
         DeclareLaunchArgument("amplitude", default_value="0.10",
